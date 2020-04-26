33d740b2d83ba9d65cbc2ae0e9cb732ce7b293d7
Damir <dlotinac@gmail.com>
2020-04-26 15:44:12 +0200

    os paths instead of string merges

--- htn/assemble.py
+++ htn/assemble.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python
-
 import subprocess
 import argparse
+import os
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='input domain')
-    parser.add_argument('-p', '--path', help='output path')
+    parser.add_argument('-i', '--input', help='input domain name')
+    parser.add_argument('-o', '--output', help='output path for generated htns')
     args = parser.parse_args()
 
-    with open('domains/'+args.input+'/'+args.input+'.spec') as f:
+    out_path = os.path.normpath(args.output)
+    spec_file_path = os.path.join("./domains",args.input,args.input+".spec")
+    prob_list_path = os.path.normpath(args.output+'/prob_list.txt')
+
+    with open(spec_file_path) as f:
         content = f.readlines()
         domain = content[0].rstrip()
         path = content[1].rstrip()
@@ -18,11 +21,11 @@ def main():
         rep_instance = content[3].rstrip()
         problems = content[4:]
 
-    domain_out = open(args.path+'/'+domain,'w')
-    domain_path = path+domain_file
-    subprocess.call(["./bin/create_htn", domain, domain_path, path+rep_instance], stdout=domain_out)
+    with open(os.path.join(out_path,domain),'w') as domain_out:
+        domain_path = path+domain_file
+        subprocess.call(["./bin/create_htn", domain, domain_path, path+rep_instance], stdout=domain_out)
 
-    with open(args.path+'/prob_list.txt','w') as prob_list:
+    with open(prob_list_path,'w') as prob_list:
         for problem in problems:
             problem = problem.rstrip()
             problem_path = path+problem
@@ -32,7 +35,7 @@ def main():
             prob_list.write(problem.rstrip('.pddl')+'\n')
             if problem == '':
                 continue
-            problem_out = open(args.path+'/'+problem.rstrip('.pddl'),'w')
+            problem_out = open(args.output+'/'+problem.rstrip('.pddl'),'w')
             subprocess.call(["./bin/initialize", domain, domain_path, problem_path], stdout=problem_out)
             problem_out.close()
     prob_list.close()
