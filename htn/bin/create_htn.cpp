1034e58d46c525334f4e37ae37759530b0f74468
Damir <dlotinac@gmail.com>
2020-05-24 09:26:40 +0200

    Finished FIKO methods

--- htn/bin/create_htn.cpp
+++ htn/bin/create_htn.cpp
@@ -221,11 +221,11 @@ void printInnerACHIEVE(Domain &d, std::ostream &out, int invariant_num, int cond
 	out << "    )\n";
 }
 
-void printHDDLInnerACHIEVE(Domain &d, std::ostream &out, int invariant_num, int condition_num, Condition& pred,
+std::string printHDDLInnerACHIEVE(Domain &d, std::ostream &out, int invariant_num, int condition_num, Condition& pred,
 		std::vector<std::string>& params) {
 	Condition c = invs[invariant_num].conds[condition_num];
 	if (isPosNegInvariant(invariant_num) && c.neg)
-		return;
+		return "";
 	std::ostringstream ts;
 	ts<<d.parametrizeHDDLCondition(c, "ACHIEVE-", invariant_num);
 	out<<"( :task "<<ts.str()<<std::endl<<")"<<std::endl;
@@ -238,6 +238,8 @@ void printHDDLInnerACHIEVE(Domain &d, std::ostream &out, int invariant_num, int 
 	out<<"\t:task"<<d.parametrizeCondition(c, "ACHIEVE-",false, invariant_num)<<std::endl;
 	out<<"\t:subtasks ( )"<<std::endl;
 	out<<")"<<std::endl;
+
+	return d.parametrizeCondition(c, "ACHIEVE-",false, invariant_num);
 }
 
 void printACHIEVEOps(Domain &d, std::ostream &out) {
@@ -891,6 +893,7 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 				}
 			}
 			for (unsigned k = 0; k < invs[i].conds.size(); ++k) {
+				std::string hddlTaskParam;
 				bool achieveWrap(false);
 				Condition pred = invs[i].conds[k];
 				if (j == k) {
@@ -898,7 +901,7 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 						printHDDLTopACHIEVE(d, out, param_types, params, i, j, pred);
 						achieveWrap = true;
 					}
-					printHDDLInnerACHIEVE(d, out, i, j, pred, params);
+					hddlTaskParam = printHDDLInnerACHIEVE(d, out, i, j, pred, params);
 				}
 				if (j != k || invs[i].types.size() < c.params.size()) {
 					x = d.pmap[pred.name];
@@ -982,14 +985,8 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 										head<<tp;
 									head<<")"<<std::endl;
 									out<<head.str();
-									//printCondition(out, false, c, "ACHIEVE", true, params, i);
-									out << "\n";
-									out << "              (";
-									
-									//sort-by
-									if(sort)
-										out << ":sort-by ?d (";
-
+									out<<"\t:task "<<hddlTaskParam<<std::endl;
+									out << "\t:precondition (and";
 									out << " ( NOT";
 									printCondition(out, false, c, "", false, params);
 									out << " )";
@@ -1001,7 +998,8 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 											if(pred == c3) continue;
 											printCondition(out, c3.neg, c3, pa, mys);
 										}
-									printTypes(out, d, a.params, pa);
+									out<<" )"<<std::endl; //end precondtion
+									//printTypes(out, d, a.params, pa);
 									// if (!isDirReachable) {
 									// 	out << " ( NOT";
                                     //     printCondition(out, false, pred, "VISITED", true, p2);
@@ -1014,16 +1012,7 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 									// 		out << " )";
 									// 	}
 									// }
-									if(sort){
-											out<<" (chksame";
-											for(int chk=0;chk<sort_by.size();++chk){
-												out<<" "<<sort_by[chk];
-											}
-											out<<" ?d )";
-											out << " )";
-										}
-
-									out << " )\n              (";
+									out<<"\t:ordered-subtasks (and ";
 									// if(!isDirReachable) {
 									// 	if (!isPosNegInvariant(i) && !pred.neg)
                                     //         printCondition(out, false, pred, "!!VISIT", true, p2);
@@ -1040,7 +1029,8 @@ void printHTN(Domain &d, Instance& ins, std::ostream &out, std::string domain_na
 									}
 
 									printCondition(out, false, c, "ACHIEVE", true, params, i);
-									out << " )\n    )\n";
+									out << " )\n)\n"; //end ordered-subtasks and end method
+
 								} else if ( i3->first == j &&  invs[i].types.size() >= invs[i].conds[j].params.size()) {
 								 	Condition add = a.getCondition( v[z].t.c );
 								 	for ( unsigned l = 0; l < add.params.size(); ++l ) {
